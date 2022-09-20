46768bddcc43c18d700f6ec31b6be62c852bee59
Hunter Downey <hdowneymac@gmail.com>
2022-09-20 17:17:28 -0500

    APICaller

--- Space Data/ContentView.swift
+++ Space Data/ContentView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    init() {
+        fetchData()
+    }
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("")
         }
         .padding()
+    }
+    
+    func fetchData() {
+        APICaller.shared.getApod { result in
+            DispatchQueue.main.async {
+                switch result {
+                    case .success(let data):
+                        var apod = Apod(
+                            copyright: data.copyright,
+                            date: data.date,
+                            explanation: data.explanation,
+                            hdurl: data.hdurl,
+                            title: data.title
+                        )
+                        print(apod.explanation)
+                    case .failure(let error):
+                        print(error.localizedDescription)
+                }
+            }
+        }
     }
 }
 
---
+++ Space Data/Models/Apod.swift
@@ -0,0 +1,17 @@
+//
+//  Apod.swift
+//  Space Data
+//
+//  Created by hunter downey on 9/20/22.
+//
+
+import Foundation
+
+struct Apod: Codable {
+    let copyright: String
+    let date: String
+    let explanation: String
+    let hdurl: String
+    let title: String
+}
+
