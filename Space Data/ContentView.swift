063ca9b506ee886bf31722492b8d5228b9cbddf7
Hunter Downey <hdowneymac@gmail.com>
2022-09-20 15:49:54 -0500

    Initial Commit

---
+++ Space Data/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Space Data
+//
+//  Created by hunter downey on 9/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Space Data/Space_DataApp.swift
@@ -0,0 +1,17 @@
+//
+//  Space_DataApp.swift
+//  Space Data
+//
+//  Created by hunter downey on 9/20/22.
+//
+
+import SwiftUI
+
+@main
+struct Space_DataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
